8d2f57f6b1cee409f70130183bf3666b8cb9a64f
Luiz Adolfo <luiz-adolfo@hotmail.com>
2024-04-27 00:54:26 -0300

    refactoring to 2.1.0

--- src/Interceptors/RedisAuthorizationInterceptor.cs
+++ src/Interceptors/RedisAuthorizationInterceptor.cs
@@ -9,23 +9,23 @@ namespace AuthorizationInterceptor.Extensions.Redis.Interceptors
     internal class RedisAuthorizationInterceptor : IAuthorizationInterceptor
     {
         private readonly IDistributedCache _cache;
-        private const string CACHE_KEY = "authorization_interceptor_redis_cache_RedisAuthorizationInterceptor";
+        private const string CACHE_KEY = "authorization_interceptor_redis_cache_RedisAuthorizationInterceptor_{0}";
 
         public RedisAuthorizationInterceptor(IDistributedCache cache)
         {
             _cache = cache;
         }
 
-        public async Task<AuthorizationHeaders?> GetHeadersAsync()
+        public async Task<AuthorizationHeaders?> GetHeadersAsync(string name)
         {
-            var data = await _cache.GetStringAsync(CACHE_KEY);
+            var data = await _cache.GetStringAsync(string.Format(CACHE_KEY, name));
             if (string.IsNullOrEmpty(data))
                 return null;
 
             return AuthorizationHeadersJsonSerializer.Deserialize(data);
         }
 
-        public async Task UpdateHeadersAsync(AuthorizationHeaders? _, AuthorizationHeaders? newHeaders)
+        public async Task UpdateHeadersAsync(string name, AuthorizationHeaders? _, AuthorizationHeaders? newHeaders)
         {
             if (newHeaders == null)
                 return;
@@ -36,7 +36,7 @@ namespace AuthorizationInterceptor.Extensions.Redis.Interceptors
                 AbsoluteExpirationRelativeToNow = newHeaders.GetRealExpiration()
             };
 
-            await _cache.SetStringAsync(CACHE_KEY, data, options);
+            await _cache.SetStringAsync(string.Format(CACHE_KEY, name), data, options);
         }
     }
 }
